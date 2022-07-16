491b927356fce45cbc35e3a2214eb1577ab530e6
Felipe Montealegre <felipemontealegre@hotmail.com>
2022-07-16 09:59:13 -0500

    Ejercicio 004 - Números Harshad

---
+++ ejercicios/004/phillmont/hashard.cs/hashard.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    int totalnumeros = 0;
+    for (int a = 1; a <= 100; a++)
+    {
+        string ta=a.ToString();
+        int l=ta.Length;
+        int suma = 0;
+        for (int i = 0; i < l; i++)
+        {
+            string c = ta.Substring(i, 1);
+            suma=suma+int.Parse(c);
+        }
+        if (a%suma == 0)
+            totalnumeros=totalnumeros+1;
+    }
+    Console.WriteLine(totalnumeros);
+}
