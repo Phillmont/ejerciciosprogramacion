c740210ccbfa96ae53952037566bc8b3f5ecf564
Felipe Montealegre <felipemontealegre@hotmail.com>
2022-07-16 11:25:12 -0500

    Ejercicio 007 - Descomposición de cadena de caracteres en pares

---
+++ ejercicios/007/phillmont/Cadena en pares/Cadena en pares/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] generarparesdelacadena(string cadena)
+{
+    int lc = cadena.Length;
+    string[] pares = new string[lc];
+    for (int i = 0; i < lc; i++)
+    {
+        pares[i] = cadena.Substring(i, 1) + ", " + cadena.Substring(0, i) + cadena.Substring(i + 1, lc - i - 1);
+    }
+    return pares;
+}
+
+Console.WriteLine("Introduzca una frase:");
+string frase = Console.ReadLine();
+Console.WriteLine();
+string[] pares = generarparesdelacadena(frase);
+foreach (string cadena in pares)
+    Console.WriteLine(cadena);
