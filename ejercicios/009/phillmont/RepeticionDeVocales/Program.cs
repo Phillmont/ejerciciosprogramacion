e677561652470a41dddaf2bda846a19e4357617d
Felipe Montealegre <felipemontealegre@hotmail.com>
2022-07-30 16:08:54 -0500

    Repetición de vocales

---
+++ ejercicios/009/phillmont/RepeticionDeVocales/Program.cs
@@ -0,0 +1,21 @@
+﻿string GenerarFraseNueva(string frase, int repeticiones)
+{
+    string nuevaFrase = "";
+    var vocales = new List<string> { "a", "e", "i", "o", "u" };
+    for (int i = 0; i < frase.Length; i++)
+    {
+        if (vocales.Contains(frase.Substring(i, 1)))
+        {
+            if (repeticiones > 0)
+            {
+                for (var numrep = 1; numrep <= repeticiones; numrep++)
+                    nuevaFrase = nuevaFrase + frase.Substring(i, 1);
+            }
+        }
+        else
+            nuevaFrase = nuevaFrase + frase.Substring(i, 1);
+    }
+    return nuevaFrase;
+}
+
+Console.WriteLine(GenerarFraseNueva("felipe", 1));
