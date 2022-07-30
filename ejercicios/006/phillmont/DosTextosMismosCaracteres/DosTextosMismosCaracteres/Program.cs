7902723559e7a030bbad8634ea46cf284c471e2d
Felipe Montealegre <felipemontealegre@hotmail.com>
2022-07-30 12:58:29 -0500

    Dos textos, mismos caracteres

---
+++ ejercicios/006/phillmont/DosTextosMismosCaracteres/DosTextosMismosCaracteres/Program.cs
@@ -0,0 +1,9 @@
+﻿bool ContienenLosMismosCaracteres (string string1,string string2)
+{
+    return string.Concat(string1.OrderBy(ch => ch)) == string.Concat(string2.OrderBy(ch => ch));
+}
+
+if (ContienenLosMismosCaracteres("A", "A") == false)
+    Console.WriteLine("OK");
+else
+    Console.WriteLine("Error");
